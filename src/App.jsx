e69bde790c707723bc4b0108404d393fde639efc
LYW14 <146258004+LYW14@users.noreply.github.com>
2025-07-30 00:20:22 -0400

    created Navbar and Hero section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <nav className='container'>
+      <img src={logo} alt="" className='logo'/>
+      <ul>
+        <li>Home</li>
+        <li>Methods</li>
+        <li>Scheduling</li>
+        <li>On Demand Learning</li>
+        <li>Other Resources</li>
+        <li><button className='btn'>Contact Us</button></li>
+
+      </ul>
+    </nav>
+  )
+}
+export default Navbar
