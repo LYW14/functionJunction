6fb552d403214049e7a6c22ae6a3777569de97a7
LYW14 <146258004+LYW14@users.noreply.github.com>
2025-08-04 17:16:27 -0400

    right column of contact section done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,6 +5,30 @@ import email_icon from '../../assets/email.svg'
 import location_icon from '../../assets/location-icon.png'
 
 const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "5539fded-e838-4d4c-a672-018527035f94");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div className='contact'>
       <div className="contact-col">
@@ -17,13 +41,21 @@ const Contact = () => {
         <ul>
             <li> <img src={email_icon} alt=''></img>
                 lyw14@bu.edu</li>
-            <li><img src={linkedin_icon} alt=''></img>
-                LinkedIn: <a href="https://www.linkedin.com/in/lyw14/">Lucy Wang</a></li>
+            <li><a href="https://www.linkedin.com/in/lyw14/"><img src={linkedin_icon} alt=''></img>
+                LinkedIn: Lucy Wang</a></li>
             <li><img src={location_icon} alt=''></img>
                 Boston, MA 02113, United States</li>
         </ul>
       </div>
-      <div className="contact-col"></div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <input type="text" name="name" placeholder='Your Name' required />
+          <input type="email" name="email" placeholder='Your Email' required />
+          <textarea name="message" rows="5" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn dark-btn'>Send Message</button>
+        </form>
+        <span></span>
+      </div>
     </div>
   )
 }
