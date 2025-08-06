8198b11bd4faff6d8fedf6c8e3ff3952b86dcfeb
LYW14 <146258004+LYW14@users.noreply.github.com>
2025-08-05 20:34:25 -0400

    added media query to all files so they are responsive to multiple screen sizes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css'
 import dark_arrow from '/Users/lucywang/Desktop/functionJunction/src/assets/dark-arrow.png'
 const Hero = () => {
   return (
-    <div className='hero container'>
+    <div className='hero'>
       <div className='hero-text'>
         <h1>Launch Your Learning 
         </h1>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,46 @@
-import React, {useEffect, useState} from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.svg'
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.svg';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
-
   const [sticky, setSticky] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      window.scrollY > 50 ? setSticky(true) : setSticky(false);
-    })
+    const handleScroll = () => {
+      setSticky(window.scrollY > 50);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  return (
-    <nav className={`container ${sticky? 'dark-nav' : ''}`}>
-      <img src={logo} alt="" className='logo'/>
-      <ul>
-        <li>Home</li>
-        <li>Methods</li>
-        <li>About</li>
-        <li>On Demand Learning</li>
-        <li>Other Resources</li>
-        <li><button className='btn'>Contact Us</button></li>
+  const toggleMenu = () => {
+    setNavOpen(prev => !prev);
+  };
 
+  return (
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+      <img src={logo} alt="logo" className="logo" />
+
+      {/* Menu Icon */}
+      <div
+        className={`menu-icon ${navOpen ? 'active' : ''}`}
+        onClick={toggleMenu}
+      ></div>
+
+      {/* Nav Links */}
+      <ul className={navOpen ? 'side-nav open' : 'side-nav'}>
+        <li><Link to="hero" smooth={true} offset={0} duration={500} onClick={() => setNavOpen(false)}>Home</Link></li>
+        <li><Link to="methods" smooth={true} offset={-300} duration={500} onClick={() => setNavOpen(false)}>Methods</Link></li>
+        <li><Link to="about" smooth={true} offset={-170} duration={500} onClick={() => setNavOpen(false)}>About</Link></li>
+        <li>On Demand Learning</li>
+        <li><Link to="testimonial" smooth={true} offset={-300} duration={500} onClick={() => setNavOpen(false)}>Testimonials</Link></li>
+        <li>Other Resources</li>
+        <li><Link to="contact" smooth={true} offset={-300} duration={500} className="btn" onClick={() => setNavOpen(false)}>Contact Us</Link></li>
       </ul>
     </nav>
-  )
-}
-export default Navbar
+  );
+};
+
+export default Navbar;
